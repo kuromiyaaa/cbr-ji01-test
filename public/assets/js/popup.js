c59f2d86436595cdf05d2f6981bb0c7dd38f990a
“Reina <reina.kuromiya@cast-er.com>
2026-01-23 09:37:01 +0900

    Update popup.js

--- public/assets/js/popup.js
+++ public/assets/js/popup.js
@@ -1,12 +1,32 @@
 $(document).ready(function () {
     var upWindow = $('#pop_up');
     var flag = true;
+    var hasShown = false; // ポップアップが一度表示されたかどうか
 
-    // 初期状態でポップアップを表示
-    upWindow.show(); 
+    // 初期状態でポップアップを非表示
+    upWindow.hide(); 
+
+    // FVセクションの高さを取得
+    var fvSection = $('.cb-fv');
+    var fvHeight = fvSection.length ? fvSection.outerHeight() : 0;
+    // FVより少し下（FVの高さ + 200px）で表示
+    var scrollThreshold = fvHeight + 200;
+
+    // スクロールイベントを監視
+    $(window).on('scroll', function () {
+        if (!hasShown && flag) {
+            var scrollTop = $(window).scrollTop();
+            
+            // スクロール位置が閾値を超えたらポップアップを表示
+            if (scrollTop > scrollThreshold) {
+                upWindow.show();
+                hasShown = true;
+            }
+        }
+    });
 
     // ✖ボタンで非表示
-    $('button').click(function () {
+    $('#pop_up button').click(function () {
         flag = false;
         $('#pop_up').hide();
     });
